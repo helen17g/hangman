fbc73b39b1383879d69d6c9821e39a0f44481750
Helen <111532399+helen17g@users.noreply.github.com>
2024-09-09 22:32:17 -0500

    Create choice.js

---
+++ hangman/choice.js
@@ -0,0 +1,15 @@
+
+function WORDchoice(){
+  noLoop();
+  let rando = floor(random(0,WORD.length + 1));
+  FirstWORD = WORD[rando];
+  console.log(FirstWORD);
+  
+  
+  FirstLength = FirstWORD.length;
+  console.log(FirstLength);
+  
+  
+  
+}
+
